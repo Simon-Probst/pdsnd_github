9642a25174fd808a4ae0da8296c454749315bba8
Simon Probst <simon.probst@hotmail.de>
2020-10-20 11:41:27 +0200

    Capitalized City Names in one line

--- US_bikeshare_SP.py
+++ US_bikeshare_SP.py
@@ -20,7 +20,7 @@ def get_filters():
     print('Hello! Let\'s explore some US bikeshare data!')
 
     # get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
-    print('Please specify if you want to analyze data for chicago, new york city or washington ?')
+    print('Please specify if you want to analyze data for Chicago, New York Yity or Washington ?')
     def cityname():
         city = str(input('Type city name :').lower())
         if city not in citynames:
